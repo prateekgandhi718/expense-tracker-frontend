c5e46fcb12f60f124f18e5fb898d523ea5471948
Prateek <prateekgandhi718@gmail.com>
2022-11-04 18:46:02 +0530

    filter

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -13,12 +13,19 @@ const Expenses = () => {
 
 
   return (
-    <div className='mx-3'>
+    <div className='mx-3 '>
       <div className='row my-3'>
         <h1 className='display-6'>Expenses.</h1>
+        <div className="btn-group container d-flex my-3" style={{ width: '30rem' }} role="group" aria-label="Basic example">
+          <button type="button" className="btn btn-light" onClick={() => { getExpenses('all') }}>See All</button>
+          <button type="button" className="btn btn-secondary" onClick={() => { getExpenses('pending') }}>See Pending</button>
+          <button type="button" className="btn btn-danger" onClick={() => { getExpenses('rejected') }}>See Rejected</button>
+          <button type="button" className="btn btn-success" onClick={() => { getExpenses('approved') }}>See Approved</button>
+        </div>
         <div className='container'>
           {expenses.length === 0 && 'You have not added any expenses yet.'}
         </div>
+
         {expenses.map((element) => {
           return <ExpenseItem key={element.id} expenseProp={element} />
         })}
